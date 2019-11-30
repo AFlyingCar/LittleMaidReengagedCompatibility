8dedda3a5952d8455593a7cf1e7869398389f45e
Tyler Robbins <tyler@familyrobbins.com>
2019-11-30 14:15:34 -0800

    Added empty event handlers so that our lang file actually gets loaded.

--- src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
+++ src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
@@ -1,8 +1,26 @@
 package com.aflyingcar.lmrcompat;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "lmrcompat", name = "LittleMaidReengaged Compatibility", version = LMRCompat.VERSION)
 public class LMRCompat {
     public static final String VERSION = "1.0.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println("LMRCompat:PreInit");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("LMRCompat:Init");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        System.out.println("LMRCompat:PostInit");
+    }
 }
