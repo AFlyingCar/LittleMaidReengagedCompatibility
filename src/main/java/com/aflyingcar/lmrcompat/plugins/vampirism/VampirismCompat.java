30f1f6b2f5a784342889cf146a6c554783956fef
Tyler Robbins <tyler@familyrobbins.com>
2020-05-26 02:37:31 -0700

    Added VampirismCompat file to handle registrations with the Vampirism API.

---
+++ src/main/java/com/aflyingcar/lmrcompat/plugins/vampirism/VampirismCompat.java
@@ -0,0 +1,31 @@
+package com.aflyingcar.lmrcompat.plugins.vampirism;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.aflyingcar.lmrcompat.LMRCompat;
+import de.teamlapen.vampirism.api.VampirismAPI;
+import net.blacklab.lmr.entity.EntityLittleMaid;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class VampirismCompat {
+    @GameRegistry.ObjectHolder("vampirism:player.bite")
+    static final SoundEvent bite_sfx = null;
+
+    public final static int REFRESH_GARLIC_TICKS = 40;
+    public final static int REFRESH_SUNDAMAGE_TICKS = 8;
+
+    public final static int SUNDAMAGE_POTION_EFFECTS_PERIOD = 40;
+
+    public static void preinit() {
+        EntityRegistry.registerModEntity(new ResourceLocation("lmreengaged", "ConvertedLittleMaid"), EntityConvertedLittleMaid.class, EntityConvertedLittleMaid.class.getSimpleName(), 0, LMRCompat.getInstance(), 50, 2, true);
+    }
+
+    public static void init() {
+        VampirismAPI.entityRegistry().addBloodValue(new ResourceLocation("lmreengaged", "LittleMaid"), EntityLittleMaidVampirism.ENTITY_MAID_BLOOD);
+        VampirismAPI.entityRegistry().addConvertible(EntityLittleMaid.class, null, new EntityConvertedLittleMaid.ConvertingHandler());
+    }
+}
