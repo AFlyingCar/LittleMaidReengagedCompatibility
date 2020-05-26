dc89ead2fd8cec5cda504d81a006dea2c6a01375
Tyler Robbins <tyler@familyrobbins.com>
2020-05-26 02:36:30 -0700

    Added an intermediary class between EntityLittleMaid and any potential subclasses we may have for vampirism.

---
+++ src/main/java/com/aflyingcar/lmrcompat/plugins/vampirism/EntityLittleMaidVampirism.java
@@ -0,0 +1,14 @@
+package com.aflyingcar.lmrcompat.plugins.vampirism;
+
+import net.blacklab.lmr.entity.EntityLittleMaid;
+import net.minecraft.world.World;
+
+public class EntityLittleMaidVampirism extends EntityLittleMaid {
+    public static final int ENTITY_MAID_BLOOD = 5;
+
+    public EntityLittleMaidVampirism(World par1World) {
+        super(par1World);
+    }
+
+    // TODO: Have some stuff here about maids trying to avoid vampires
+}
