92b07f0b2ee47d9002807c238a8b5472d6a9bd11
Tyler Robbins <tyler@familyrobbins.com>
2020-06-28 05:12:27 -0700

    Added code to only register compatibility modes if the respective mods are loaded. Began working on converting maids to vampires with an injection of sanguinare. Began working on the ability for maids to be vampire hunters.

--- src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
+++ src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
@@ -1,16 +1,28 @@
 package com.aflyingcar.lmrcompat;
 
+import com.aflyingcar.lmrcompat.plugins.vampirism.EntityLittleMaidVampirism;
 import com.aflyingcar.lmrcompat.plugins.vampirism.VampirismCompat;
 import com.aflyingcar.lmrcompat.proxies.CommonProxy;
+import de.teamlapen.vampirism.core.ModItems;
+import de.teamlapen.vampirism.items.ItemInjection;
+import de.teamlapen.vampirism.potion.PotionSanguinare;
+import de.teamlapen.vampirism.potion.PotionSanguinareEffect;
+import net.blacklab.lmr.api.mode.EntityModeRegistrar;
+import net.blacklab.lmr.entity.EntityLittleMaid;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.EnumActionResult;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -73,8 +85,17 @@ public class LMRCompat {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
+        if(hasTechGuns) {
+            EntityModeRegistrar.registerMaidMode(EntityMode_Gunslinger.class);
+        }
+
+        if(hasChickens && hasRoost) {
+            EntityModeRegistrar.registerMaidMode(EntityMode_ChickenBreeder.class);
+        }
+
         if(hasVampirism) {
             VampirismCompat.init();
+            // EntityModeRegistrar.registerMaidMode(EntityMode_VampireHunter.class);
         }
 
         proxy.init();
@@ -97,6 +118,32 @@ public class LMRCompat {
         public static void onRegisterRecipes(RegistryEvent.Register<IRecipe> event) {
             logger.info("Registering all recipes.");
             LMRCompatRecipes.registerAll(event.getRegistry());
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID)
+    public static class EventHandler {
+        @SubscribeEvent(priority = EventPriority.HIGH)
+        public static void onRightClick(PlayerInteractEvent.EntityInteract event) {
+            if(!hasVampirism) return;
+
+            ItemStack stack = event.getItemStack();
+            if(!stack.isEmpty() && stack.getItem().equals(ModItems.injection))
+            {
+                if(event.getTarget() instanceof EntityLittleMaid && !(event.getTarget() instanceof EntityLittleMaidVampirism)) {
+                    if(stack.getMetadata() == ItemInjection.META_GARLIC) {
+                        // TODO: Convert Maid to a hunter variant (disables ability to become a vampire)
+                        //    Enable stake job???
+                    } else if(stack.getMetadata() == ItemInjection.META_SANGUINARE) {
+                        // TODO: This for some reason spawns an empty husk ???
+                        //  It's not a real maid, that's for certain
+                        /*
+                            PotionSanguinare.addRandom((EntityLivingBase) event.getTarget(), false);
+                            event.setCancellationResult(EnumActionResult.SUCCESS);
+                         */
+                    }
+                }
+            }
         }
     }
 }
