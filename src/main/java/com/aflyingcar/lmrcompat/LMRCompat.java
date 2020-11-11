a7f5878132b7c0dee7acb85da760e01b358e69b0
Tyler Robbins <tyler@familyrobbins.com>
2020-11-10 18:49:01 -0800

    Updated version number.

--- src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
+++ src/main/java/com/aflyingcar/lmrcompat/LMRCompat.java
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Logger;
 public class LMRCompat {
     public static final String MODID = "lmrcompat";
     public static final String MODNAME = "LittleMaidReengaged Compatibility";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String DEPENDENCIES = "after:vampirism;after:techguns;after:chickens;after:roost";
 
     private static Logger logger;
